b8310d71c5c5803efab985389f3b58cedda01d7b
muhreynaldii <reynaldi180101@gmail.com>
2022-03-24 23:26:54 +0700

    using express-ejs-layout

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,42 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res) {
-  res.render("index");
+  res.render("index", { layout: false });
 });
 
+router.get("/login", function (req, res) {
+  res.render("login", {
+    layout: false,
+    title: "Login",
+    judul: false,
+    subjudul: false,
+  });
+});
 router.get("/dashboard", function (req, res) {
-  res.render("dashboard");
+  res.render("dashboard", {
+    layout: "./layouts/layout",
+    title: "Dashboard",
+    judul: "DASHBOARD",
+    subjudul: "Dashboard",
+  });
 });
 
-router.get("/cars", function (req, res) {
-  res.render("cars");
+router.get("/listcars", function (req, res) {
+  res.render("listCars", {
+    layout: "./layouts/layout",
+    title: "List Cars",
+    judul: "CARS",
+    subjudul: "List Cars",
+  });
 });
 
 router.get("/addNewCar", function (req, res) {
-  res.render("addNewCar");
+  res.render("addNewCar", {
+    layout: "./layouts/layout",
+    title: "Add New Car",
+    judul: "CARS",
+    subjudul: "List Cars",
+  });
 });
 
 module.exports = router;
